2d2fb6e9ac6fc51f9e85236f5171c01386557682
czheisenberg <czheisenberg@qq.com>
2024-10-27 14:52:30 +0800

    feat: 首页

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.text}>首页</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#25292e',
+    },
+    text: {
+      color: '#fff',
+    }
+})
