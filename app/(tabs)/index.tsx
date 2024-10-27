922396764b4762a3269722a74777ea88d21dd32a
czheisenberg <czheisenberg@qq.com>
2024-10-27 16:31:48 +0800

    feat: 构建屏幕

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,23 @@
 import { Text, View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
+import { Image } from "expo-image";
+import ImageViewer from "../components/ImageViewer";
+import Button from "../components/Button";
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View
       style={styles.container}
     >
-      <Text style={styles.text}>首页</Text>
-      <Link
-        href="/about"
-        style={styles.button}
-      >
-        关于我们
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="选择图片" theme="primary" />
+        <Button label="使用这张" />
+      </View>
     </View>
   );
 }
@@ -31,5 +36,12 @@ const styles = StyleSheet.create({
       fontSize: 20,
       color: '#fff',
       textDecorationLine: 'underline'
+    },
+    imageContainer: {
+      flex: 1
+    },
+    footerContainer: {
+      flex: 1/3,
+      alignItems: 'center'
     }
 })
